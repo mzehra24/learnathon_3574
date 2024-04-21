dad71eafb75259a89fffe21b970f23be0f1554c3
mzehra24 <154894760+mzehra24@users.noreply.github.com>
2024-04-21 17:49:56 +0530

    day 20 math_operations.py

---
+++ day 20 math_operations.py
@@ -0,0 +1,12 @@
+Create a Python module named math_operations containing functions to perform basic mathematical operations. Define functions for addition, subtraction, multiplication, and division. Additionally, include a constant PI representing the value of π (pi). Save this module in a file named math_operations.py.
+
+#save the name of the file as math_operations.py
+def add(x,y):
+  return x+y
+def sub(x,y):
+  return x-y
+def mul(x,y):
+  return x*y
+def div(x,y):
+  return x/y
+pi=3.14
