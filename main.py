8ed8835dd4d27b0fe5f02c43db25021febd282fe
mzehra24 <154894760+mzehra24@users.noreply.github.com>
2024-04-21 17:10:36 +0530

    Add files via upload

---
+++ main.py
@@ -0,0 +1,8 @@
+import math_operations
+print("Addition:", math_operations.add(5, 3))
+print("Subtraction:", math_operations.sub(5, 3))
+print("Multiplication:", math_operations.mul(5, 3))
+print("Division:", math_operations.div(5, 3))
+
+# Demonstrating usage of constant
+print("Value of PI:", math_operations.PI)
